d9fa871c23fea7d199de52f3d8932e180006b298
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2022-03-28 00:22:20 +0530

    Update ZigZag_Traversal.java

--- core/ZigZag_Traversal.java
+++ core/ZigZag_Traversal.java
@@ -1,126 +1,5 @@
 /*https://practice.geeksforgeeks.org/problems/zigzag-tree-traversal/1#*/
-
-    // { Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.util.HashMap;
-import java.io.*;
-
-    class Node
-    {
-        int data;
-        Node left,right;
-        Node(int data){
-            this.data = data;
-            left=null;
-            right=null;
-        }
-    }
-public class ZigZag_Traversal
-
-    {
-        static Node buildTree(String str){
-
-            if(str.length()==0 || str.charAt(0)=='N'){
-                return null;
-            }
-
-            String ip[] = str.split(" ");
-            // Create the root of the tree
-            Node root = new Node(Integer.parseInt(ip[0]));
-            // Push the root to the queue
-
-            Queue<Node> queue = new LinkedList<>();
-
-            queue.add(root);
-            // Starting from the second element
-
-            int i = 1;
-            while(queue.size()>0 && i < ip.length) {
-
-                // Get and remove the front of the queue
-                Node currNode = queue.peek();
-                queue.remove();
-
-                // Get the current node's value from the string
-                String currVal = ip[i];
-
-                // If the left child is not null
-                if(!currVal.equals("N")) {
-
-                    // Create the left child for the current node
-                    currNode.left = new Node(Integer.parseInt(currVal));
-                    // Push it to the queue
-                    queue.add(currNode.left);
-                }
-
-                // For the right child
-                i++;
-                if(i >= ip.length)
-                    break;
-
-                currVal = ip[i];
-
-                // If the right child is not null
-                if(!currVal.equals("N")) {
-
-                    // Create the right child for the current node
-                    currNode.right = new Node(Integer.parseInt(currVal));
-
-                    // Push it to the queue
-                    queue.add(currNode.right);
-                }
-                i++;
-            }
-
-            return root;
-        }
-
-        public static void inorder(Node root)
-        {
-            if(root==null)
-                return;
-            inorder(root.left);
-            System.out.print(root.data);
-            inorder(root.right);
-        }
-        /* Drier program to test above functions */
-        public static void main(String args[]) throws IOException
-        {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            int t=Integer.parseInt(br.readLine());
-
-            while(t > 0){
-                String s = br.readLine();
-                Node root = buildTree(s);
-                GFG g = new GFG();
-
-                ArrayList<Integer> res = g.zigZagTraversal(root) ;
-                for (int i = 0; i < res.size (); i++)
-                    System.out.print (res.get (i) + " ");
-                System. out. println();
-
-                t--;
-
-            }
-        }
-    }// } Driver Code Ends
-
-
-//User function Template for Java
-
-/*class Node
-{
-    int data;
-    Node left,right;
-    Node(int d)
-    {
-        data=d;
-        left=right=null;
-    }
-}*/
+/*https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/*/
 
     class GFG
     {
@@ -156,4 +35,46 @@ public class ZigZag_Traversal
             return l1;
         }
     }
+
+
+//leetcode
+
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> l1=new LinkedList<>();
+        if(root==null)
+                return l1;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int left_to_right=0;
+        while(!q.isEmpty()){
+            if(left_to_right==1)
+                left_to_right=0;
+            else
+                left_to_right=1;
+            List<Integer> l2=new LinkedList<>();
+            int levelsize=q.size();
+            for(int i=0;i<levelsize;i++){
+            TreeNode curr=q.poll();
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                q.add(curr.right);
+            
+            }
+            l2.add(curr.val);
+        }
+        if(left_to_right==0){
+            Collections.reverse(l2);
+            l1.add(l2);
+        }
+        else{
+            l1.add(l2);
+        }
+    }
+        return l1;
+}
+}
 
