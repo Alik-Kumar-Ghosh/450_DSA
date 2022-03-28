c38c9b1908c70ff20b032f016590d55f44960626
Alik Kumar Ghosh <33929979+Alik-Kumar-Ghosh@users.noreply.github.com>
2022-03-28 19:19:46 +0530

    Update Boundary_traversal_binary_tree.java

--- core/Boundary_traversal_binary_tree.java
+++ core/Boundary_traversal_binary_tree.java
@@ -1,6 +1,73 @@
 
 //https://practice.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1
 
+//approach 1;
+
+
+class Solution
+{   
+	ArrayList <Integer> boundary(Node root)
+	{ArrayList <Integer> arr=new ArrayList<>();
+	    if(root==null)
+            return arr;
+        arr.add(root.data);
+        leftnodes(root.left,arr);
+        leafnodes(root.left,arr);
+        leafnodes(root.right,arr);
+        rightnodes(root.right,arr);
+        //System.out.println(arr.toString());
+        return arr;
+	}
+
+     void leftnodes(Node root,ArrayList<Integer> arr){
+    	if(root==null)
+        	return; 
+        if(root.left==null&& root.right==null){
+            return;
+        }
+        arr.add(root.data);
+        if(root.left!=null)
+        leftnodes(root.left,arr);
+        else
+        leftnodes(root.right,arr);
+        //return;
+    }
+  void leafnodes(Node root,ArrayList<Integer> ans){
+    if(root==null){
+           return;
+       }
+       if(root.left==null && root.right==null){
+           ans.add(root.data);
+       }
+       leafnodes(root.left,ans);
+       leafnodes(root.right,ans);
+        //return;
+    }
+ void rightnodes(Node root,ArrayList<Integer> arr){
+    	if(root==null) 
+        	return;
+        if(root.left==null&& root.right==null){
+        	return;
+        }
+        //rightnodes(root.left);
+        if(root.right!=null)
+       	rightnodes(root.right,arr);
+       	else
+       	rightnodes(root.left,arr);
+        arr.add(root.data);
+        //return;
+    }
+}
+
+
+
+
+
+
+
+
+
+// approach 2
 public class Boundary_traversal_binary_tree {
 
     class Solution
